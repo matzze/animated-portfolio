cfd50f03899f6b3a0a64d3f2937713b0bfb366e7
matzze <64369440+matzze@users.noreply.github.com>
2024-05-31 22:52:08 +0200

    beforeMediaQuieries

--- src/components/overviewStudios/studios.jsx
+++ src/components/overviewStudios/studios.jsx
@@ -9,23 +9,23 @@ const Studios = () => {
     <div className="studios-outer-container">
       <div className="studios-inner-container">
         {/* Erste Spalte */}
-        <div className="column">
+        <div className="waldsassen-column">
           <img src={picWasa} alt="Bild1" />
-          <h2>Studio Waldsassen</h2>
-          <p>
+          <h2 className="waldsassen-h2">Studio Waldsassen</h2>
+          <p className="waldsassen-p">
             <strong>Fun-Outdoor-Area:</strong> Genießen Sie das Training an der
             frischen Luft in unserer modernen Fun-Outdoor-Area. Ob Cardio,
             Krafttraining oder funktionelles Training - hier finden Sie
             vielfältige Möglichkeiten, Ihr Workout abwechslungsreich zu
             gestalten.
           </p>
-          <p>
+          <p className="waldsassen-p">
             <strong>Ausreichend Parkplätze:</strong> Kein Stress bei der
             Parkplatzsuche! Unser Fitnessstudio verfügt über zahlreiche
             Parkplätze direkt vor der Tür. So können Sie entspannt ankommen und
             sich voll und ganz auf Ihr Training konzentrieren.
           </p>
-          <p>
+          <p className="waldsassen-p">
             <strong>24 Stunden geöffnet:</strong> Flexibilität wird bei uns
             großgeschrieben. Unser Fitnessstudio ist rund um die Uhr geöffnet,
             damit Sie jederzeit trainieren können - egal ob früh morgens, spät
@@ -34,20 +34,20 @@ const Studios = () => {
         </div>
 
         {/* Zweite Spalte */}
-        <div className="column">
-          <h2>Studio Tirschenreuth</h2>
-          <p>
+        <div className="tirschenreuth-column">
+          <h2 className="tirschenreuth-h2">Studio Tirschenreuth</h2>
+          <p className="tirschenreuth-p">
             <strong>Solarium:</strong> Gönnen Sie sich nach dem Training eine
             entspannende Auszeit in unserem Solarium. Bitte beachten Sie, dass
             die Nutzung des Solariums gegen eine zusätzliche Gebühr erfolgt.
           </p>
-          <p>
+          <p className="tirschenreuth-p">
             <strong>Vibraplate inklusive:</strong> Nutzen Sie unsere moderne
             Vibraplate, um Ihr Training zu intensivieren. Die Nutzung der
             Vibraplate ist für unsere Mitglieder kostenlos und jederzeit
             verfügbar.
           </p>
-          <div className="video-container">
+          <div className="tirschenreuth-video-container">
             <video autoPlay muted loop>
               <source src="../../../videos/GymBodyStyle.MP4" type="video/mp4" />
               Your browser does not support the video tag.
@@ -56,18 +56,17 @@ const Studios = () => {
         </div>
 
         {/* Dritte Spalte */}
-        <div className="column new-studio">
+        <div className="new-studio-column">
           <img src={picNewStudio} alt="Bild3" />
-          <h2>Neues Studio</h2>
-          <p>
+          <h2 className="new-studio-h2">Neues Studio</h2>
+          <p className="new-studio-p">
             Wir freuen uns, Ihnen bald unser brandneues Fitnessstudio
             präsentieren zu können. Bleiben Sie gespannt - weitere Informationen
             folgen in Kürze!
           </p>
-          <button className="golden-button">
+          <button className="new-studio-button">
             Jetzt Probetraining vereinbaren
           </button>
-          <div className="arrow"></div>
         </div>
       </div>
     </div>
